66b79d851699539fe8685f36aa6a6c86eed7cdbd
jonatan <jonatan@jonatanLara>
2015-05-14 21:10:55 -0500

    Manejo de Eventos

---
+++ src/testvisordefuentes/TestVisorDeFuentes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testvisordefuentes;
+
+import gui.MainForm;
+
+/**
+ *
+ * @author jonatanLara
+ */
+public class TestVisorDeFuentes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainForm Ventana = new MainForm();
+        Ventana.setVisible(true);
+    }
+    
+}
